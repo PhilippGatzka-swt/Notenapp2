e337979b56714a8836584ba2e4ae0f7d9528977c
gatzka <gatzka@sowatec.com>
2021-08-11 09:32:31 +0200

    Semester

--- app/src/main/java/com/sowatec/pg/notenapp/room/dao/SemesterDao.java
+++ app/src/main/java/com/sowatec/pg/notenapp/room/dao/SemesterDao.java
@@ -1,5 +1,6 @@
 package com.sowatec.pg.notenapp.room.dao;
 
+import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
@@ -8,6 +9,7 @@ import com.sowatec.pg.notenapp.room.entity.Semester;
 
 import java.util.List;
 
+@Dao
 public interface SemesterDao {
     @Query("SELECT * FROM semester")
     List<Semester> selectAll();
